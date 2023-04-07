cbd66112c5c7e5d5f950bc8f640794d12651f803
Oliver Grüttner <gruettner@protonmail.com>
2023-04-07 23:23:03 +0200

    fix: eslint --fix

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./popup.css";
-import Popup from "../components/Popup";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './popup.css'
+import Popup from '../components/Popup'
 
 const container = document.getElementById('chatcrafter-popup')
 if (container == null) {
   throw new Error('Root element not found')
 }
-const root = ReactDOM.createRoot(container);
-root.render(<Popup num={12334} />);
+const root = ReactDOM.createRoot(container)
+root.render(<Popup num={12334} />)
