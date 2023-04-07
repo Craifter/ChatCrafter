34e641589a4e7159d50824ff3fb00d926b18bb55
Oliver Grüttner <gruettner@protonmail.com>
2023-04-08 00:48:18 +0200

    fix: changed loading order of the sidebar

--- src/chat-gpt.ts
+++ src/chat-gpt.ts
@@ -2,7 +2,7 @@ import { injectDescription, injectSidebar } from './chat-gpt/injectors'
 
 console.log('ChatCrafter injected')
 
+injectSidebar()
 setTimeout(() => {
-  injectSidebar()
   injectDescription('ChatCrafter')
 }, 1000)
