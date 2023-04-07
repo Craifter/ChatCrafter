4b5695b76129ff0539d322bdb9c5c90bd82d0cdb
Oliver Grüttner <gruettner@protonmail.com>
2023-04-07 20:38:40 +0200

    feat: options page

---
+++ src/options/options.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./options.css";
+import { App } from "../components/App";
+
+const container = document.getElementById("chatcrafter-options")
+if (!container) {
+  throw new Error("Root element not found");
+}
+const root = ReactDOM.createRoot(container);
+root.render(<App num={1234} />);
