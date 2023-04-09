bfdd74e96f76a6ed31f92a66d54357b3b2ae3bd6
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 04:13:35 +0200

    feat: Refactor PopupContent component to use Button component and accept links prop

--- src/popup/components/PopupContent.tsx
+++ src/popup/components/PopupContent.tsx
@@ -1,49 +1,25 @@
-// Import necessary libraries
-import React, { type FC } from 'react'
-import {
-  IconList,
-  IconRobot, IconSettings
-} from '@tabler/icons-react'
+import React, { type FC, type ReactNode } from 'react'
+import { Button } from '../../components/Button'
 
-// Define the props interface
-interface Props {
-  onOpenChat: () => void
-  onOpenOptions: () => void
-  onOpenPromptRepo: () => void
+export interface PopupContentPropsLinks {
+  menuName: string
+  menuIcon: ReactNode
+  onClick: () => void
 }
 
-// Create the component
-export const PopupContent: FC<Props> = ({ onOpenChat, onOpenOptions, onOpenPromptRepo }) => {
+interface PopupContentProps {
+  links: PopupContentPropsLinks[]
+}
+
+export const PopupContent: FC<PopupContentProps> = ({ links }) => {
   return (
     <div className="bg-white shadow-md rounded p-4 w-full">
       <div className="flex flex-col space-y-4">
-        <div className="flex items-center space-x-2">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={onOpenChat}
-          >
-            <IconRobot size={16} />
-          </button>
-          <span className="text-gray-800">Open Chat</span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={onOpenOptions}
-          >
-            <IconSettings size={16} />
-          </button>
-          <span className="text-gray-800">Open Options</span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={onOpenPromptRepo}
-          >
-            <IconList size={16} />
-          </button>
-          <span className="text-gray-800">Open Prompt Repo</span>
-        </div>
+        {links.map((link) => (
+          <Button onClick={link.onClick} icon={link.menuIcon} key={link.menuName}>
+            {link.menuName}
+          </Button>
+        ))}
       </div>
     </div>
   )
