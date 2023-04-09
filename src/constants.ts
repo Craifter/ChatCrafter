2e75a0e0cc4b4f5614142ff4dbe6e8c849759e56
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 02:43:25 +0200

    feat: added icon size

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const EXAMPLE_PROMPTS_URL = 'https://github.com/Craifter/oprm/blob/main/examples/examples.oprm'
 export const CHATGTP_URL = 'https://chat.openai.com/chat'
 export const SUPPORT_CHATCRAFTER = 'https://youtu.be/dQw4w9WgXcQ'
+export const ICON_SIZE = 16
