f2a74254dd939565efff67dde6cdb5dd30a49fcb
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 05:20:16 +0200

    feat: add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,ts,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./src/**/*.{html,ts,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
