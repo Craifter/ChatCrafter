d37438f4af6d5ecd62041947c6cc7eb7c1585295
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 04:11:18 +0200

    chore: changed Popup to PopupApp

---
+++ src/popup/PopupApp.tsx
@@ -0,0 +1,33 @@
+import React, { type ReactElement } from 'react'
+import { PopupLayout } from './components/PopupLayout'
+import { PopupHeader } from './components/PopupHeader'
+import { PopupContent, type PopupContentPropsLinks } from './components/PopupContent'
+import browser from 'webextension-polyfill'
+import { CHATGTP_URL, EXAMPLE_PROMPTS_URL, ICON_SIZE } from '../constants'
+import { IconList, IconRobot, IconSettings } from '@tabler/icons-react'
+
+const links: PopupContentPropsLinks[] = [
+  {
+    menuName: 'Open ChatGPT',
+    menuIcon: <IconRobot size={ICON_SIZE} />,
+    onClick: () => { window.open(CHATGTP_URL) }
+  },
+  {
+    menuName: 'Open Options',
+    menuIcon: <IconSettings size={ICON_SIZE} />,
+    onClick: async () => { await browser.runtime.openOptionsPage() }
+  },
+  {
+    menuName: 'Open Prompt Repo',
+    menuIcon: <IconList size={ICON_SIZE} />,
+    onClick: () => { window.open(EXAMPLE_PROMPTS_URL) }
+  }
+]
+
+export default (): ReactElement => {
+  return (
+    <PopupLayout header={<PopupHeader title={'ChatCrafter'}></PopupHeader>}>
+      <PopupContent links={links} />
+    </PopupLayout>
+  )
+}
