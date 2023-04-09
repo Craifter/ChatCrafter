4abda86291b688feb2afd3174885fc455e97d594
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 06:32:49 +0200

    feat: underline for active element

--- src/options/components/OptionsContent.tsx
+++ src/options/components/OptionsContent.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, type ReactNode } from 'react'
+import React, { type FC, type ReactNode, useState } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Button } from '../../components/Button'
 import { IconExternalLink } from '@tabler/icons-react'
@@ -22,16 +22,22 @@ export interface OptionContentProps {
   externals: OptionContentPropsExternal[]
 }
 export const OptionsContent: FC<OptionContentProps> = ({ pages, externals }) => {
+  const locationHash = window.location.hash
+  const [activeTab, setActiveTab] = useState((locationHash !== '') ? locationHash.replace('#/', '') : pages[0].hash)
+
+  const handleTabClick = (hash: string): void => {
+    setActiveTab(hash)
+    window.location.hash = '/' + hash
+  }
   return (
     <div className="bg-white dark:bg-opacity-10 shadow dark:shadow-neutral-800 rounded p-4 w-full">
       <div className="scrollbar flex flex-nowrap overflow-x-auto gap-2 pb-2 mb-4">
         {pages.map((page) => (
           <Button
             key={page.menuName}
-            onClick={() => {
-              window.location.hash = '/' + page.hash
-            }}
+            onClick={() => { handleTabClick(page.hash) }}
             icon={page.menuIcon}
+            extendButtonClass={activeTab === page.hash ? 'underline underline-offset-2' : ''}
           >
             {page.menuName}
           </Button>
