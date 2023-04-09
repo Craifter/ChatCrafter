b92f6308f1eb07fd0601d94cf448c18cc7f1c00c
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 06:30:40 +0200

    feat: optional prop extendButtonClass for the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,13 +7,15 @@ export interface ButtonProps {
   icon?: ReactNode
   /** Button content */
   children: ReactNode
+  /** Additional class name to extend the button */
+  extendButtonClass?: string
 }
 
 /** A button for the options page */
-export const Button: FC<ButtonProps> = ({ onClick, icon, children }: ButtonProps) => {
+export const Button: FC<ButtonProps> = ({ onClick, icon, children, extendButtonClass }: ButtonProps) => {
   return (
     <button
-      className="flex bg-blue-500 dark:bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className={`flex bg-blue-500 dark:bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${extendButtonClass}`}
       onClick={onClick}
     >
       {icon}
