3adc6361d5bf0395efb52b41514d1471a5304236
Oliver Grüttner <gruettner@protonmail.com>
2023-04-09 04:54:49 +0200

    refactor: better usage of localStorage

--- src/utils/setDarkMode.ts
+++ src/utils/setDarkMode.ts
@@ -1,7 +1,16 @@
+/** sets the dark mode for the application based on the user's preference */
 export const setDarkMode = (): void => {
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  if (localStorage.getItem('theme') === 'dark') {
     document.documentElement.classList.add('dark')
-  } else {
+    return
+  }
+
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+  } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+    document.documentElement.classList.remove('dark')
+  } else if (window.matchMedia('(prefers-color-scheme: no-preference)').matches) {
     document.documentElement.classList.remove('dark')
   }
 }
