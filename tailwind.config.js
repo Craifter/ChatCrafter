87c4f44075bb52965dc27b27dd863a2c32c1ebfd
Oliver Grüttner <gruettner@protonmail.com>
2023-04-11 01:01:38 +0200

    chore: higher breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+        '4xl': '1920px',
+      }
+    },
   },
   plugins: [],
 }
