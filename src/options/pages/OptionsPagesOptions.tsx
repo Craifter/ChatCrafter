05ee32cd609037dc47bf2a296f17277f6cef149f
Oliver Grüttner <gruettner@protonmail.com>
2023-04-12 01:52:20 +0200

    feat: added white browser icon support

--- src/options/pages/OptionsPagesOptions.tsx
+++ src/options/pages/OptionsPagesOptions.tsx
@@ -1,53 +1,60 @@
-import React, { type FC } from 'react'
-import { Field, Form, Formik, type FormikErrors } from 'formik'
+import React, { type FC, useEffect } from 'react'
+import { type FormikErrors, useFormik } from 'formik'
 import { Button } from '../../components/Button'
+import Browser from 'webextension-polyfill'
 
 export interface OptionsPageOptionsProps {
 
 }
 
 interface OptionsFormValues {
-  darkBrowserIcons: boolean
+  whiteBrowserIcons: boolean
 }
 
 export const OptionsPagesOptions: FC<OptionsPageOptionsProps> = () => {
-  const initialValues: OptionsFormValues = {
-    darkBrowserIcons: false
-  }
+  const formik = useFormik({
+    initialValues: {
+      whiteBrowserIcons: false
+    },
+    validate: (values) => {
+      const errors: FormikErrors<OptionsFormValues> = {}
+      if (values.whiteBrowserIcons === undefined) errors.whiteBrowserIcons = '- Required'
+      return errors
+    },
+    onSubmit: async (values, actions) => {
+      await Browser.storage.sync.set({ syncOptions: values })
+      actions.setSubmitting(false)
+    }
+  })
+
+  useEffect(() => {
+    void (async () => {
+      const { syncOptions } = await Browser.storage.sync.get('syncOptions')
+      if (syncOptions.whiteBrowserIcons !== undefined) {
+        await formik.setFieldValue('whiteBrowserIcons', syncOptions.whiteBrowserIcons)
+      }
+    })()
+  }, [])
 
   return (
     <div className={'max-w-4xl mx-auto p-4 bg-white dark:bg-gray-900 rounded-lg shadow-lg text-base dark:text-white'}>
       <h1 className={'text-xl font-bold dark:text-white'}>Browser Extension Options</h1>
-      <Formik
-        initialValues={initialValues}
-        validate={(values) => {
-          const errors: FormikErrors<OptionsFormValues> = {}
-          if (!values.darkBrowserIcons) {
-            errors.darkBrowserIcons = 'Required'
-          }
-          return errors
-        }}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions })
-          actions.setSubmitting(false)
-        }}
-      >
-        {({ values }) => (
-          <Form className={'dark:text-white text-lg'}>
-            <label htmlFor="darkBrowserIcons" className={'flex gap-2'}>
-              <div>
-                <Field type="checkbox" name="darkBrowserIcons" id="darkBrowserIcons" />
-              </div>
-              <span className={'pt-0.5'}>Dark Browser Icons</span>
-            </label>
-            <div className={'flex flex-row-reverse gap-2'}>
-              <div>
-                <Button>Submit</Button>
-              </div>
+        <form onSubmit={formik.handleSubmit} className={'dark:text-white text-lg'}>
+          <label htmlFor="whiteBrowserIcons" className={'flex gap-2'}>
+            <div>
+              <input type="checkbox" name="whiteBrowserIcons" id="whiteBrowserIcons" onChange={formik.handleChange} checked={formik.values.whiteBrowserIcons} />
             </div>
-          </Form>
-        )}
-      </Formik>
+            <span className={'pt-0.5'}>
+              White Extension Icon
+              {(formik.errors.whiteBrowserIcons != null) ? <span className={'text-red-500'}> {formik.errors.whiteBrowserIcons}</span> : null}
+            </span>
+          </label>
+          <div className={'flex flex-row-reverse gap-2'}>
+            <div>
+              <Button type='submit'>Submit</Button>
+            </div>
+          </div>
+      </form>
     </div>
   )
 }
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,13 +1,32 @@
 import browser from 'webextension-polyfill'
 
-const main = async (): Promise<void> => {
-  browser.runtime.onInstalled.addListener(() => {
-    browser.contextMenus.create({
-      id: 'sampleContextMenu',
-      title: 'Sample Context Menu',
-      contexts: ['link']
-    })
+const setBrowserIcons = async (isBlack: boolean = true): Promise<void> => {
+  const suffix = isBlack ? 'black' : 'white'
+  await browser.action.setIcon({
+    path: {
+      16: `assets/logo/16-${suffix}.png`,
+      32: `assets/logo/32-${suffix}.png`,
+      48: `assets/logo/48-${suffix}.png`,
+      128: `assets/logo/128-${suffix}.png`
+    }
   })
+}
+
+const setupConfig = async (): Promise<void> => {
+  const { syncOptions } = await browser.storage.sync.get('syncOptions')
+  if (syncOptions.whiteBrowserIcons === true) { void setBrowserIcons(false) }
+}
+
+const onStorageSyncChange = async (changes: Record<string, browser.Storage.StorageChange>): Promise<void> => {
+  if (changes.syncOptions?.newValue !== undefined) {
+    const options = changes.syncOptions.newValue
+    if (options.whiteBrowserIcons === true) { void setBrowserIcons(false) } else { void setBrowserIcons() }
+  }
+}
+
+const main = async (): Promise<void> => {
+  await setupConfig()
+  browser.storage.onChanged.addListener(onStorageSyncChange)
 }
 
 main().catch(console.error)
