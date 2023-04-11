9a4e2be42b3cd47117f8aa49b72c8c89f4b38687
Oliver Grüttner <gruettner@protonmail.com>
2023-04-11 02:00:17 +0200

    chore: add a card around the content

--- src/options/pages/OptionsPagesAbout.tsx
+++ src/options/pages/OptionsPagesAbout.tsx
@@ -1,11 +1,15 @@
 import React, { type FC } from 'react'
 
 export interface OptionsPageAboutProps {
+  title: string
   description: string
 }
 
-export const OptionsPagesAbout: FC<OptionsPageAboutProps> = ({ description }) => {
+export const OptionsPagesAbout: FC<OptionsPageAboutProps> = ({ description, title }) => {
   return (
-    <p className={'text-base dark:text-white'}>{description}</p>
+    <div className={'max-w-4xl mx-auto p-4 bg-white dark:bg-gray-900 rounded-lg shadow-lg text-base dark:text-white'}>
+      <h1 className={'text-xl font-bold dark:text-white'}>{title}</h1>
+      <p className={'pt-2 text-base dark:text-white'}>{description}</p>
+    </div>
   )
 }
