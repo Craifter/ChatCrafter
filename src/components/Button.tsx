8fde656963376047d5b3439e28bc80e1ebb82bbc
Oliver Grüttner <gruettner@protonmail.com>
2023-04-12 01:51:25 +0200

    chore: added optional type parameter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,15 +9,19 @@ export interface ButtonProps {
   children?: ReactNode
   /** Additional class name to extend the button */
   extendButtonClass?: string
+  /** Button type */
+  type?: 'submit' | 'button' | 'reset'
 }
 
 /** A button for the options page */
-export const Button: FC<ButtonProps> = ({ onClick, icon, children, extendButtonClass }: ButtonProps) => {
+export const Button: FC<ButtonProps> = ({ onClick, icon, children, extendButtonClass, type }: ButtonProps) => {
   if (extendButtonClass === undefined) extendButtonClass = ''
+  if (type === undefined) type = 'button'
   return (
     <button
       className={`flex bg-blue-500 dark:bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${extendButtonClass}`}
       onClick={onClick}
+      type={type}
     >
       {icon}
       <span className="ml-1 text-base leading-4 whitespace-nowrap">{children}</span>
