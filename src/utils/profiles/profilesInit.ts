6331253d361fb2c2e0fe63fe7062748f6d098084
Oliver Grüttner <gruettner@protonmail.com>
2023-04-16 03:09:41 +0200

    chore: better test values

--- src/utils/profiles/profilesInit.ts
+++ src/utils/profiles/profilesInit.ts
@@ -1,5 +1,23 @@
 import { DEFAULT_PROMPTS } from '../../constants';
 import { profilesStorageGet, profilesStorageSet } from './profilesStorage';
+import { type Prompt } from '../../types/prompt';
+
+function getExample (): Prompt {
+  return {
+    id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+    name: 'name',
+    description: 'The name of the person you are talking to.',
+    prompt: 'Hi, my name is {{name}}.',
+    variables: [],
+    tags: [],
+    metadata: {
+      author: 'Craifter',
+      creation_date: '1997-08-29',
+      source: ''
+    },
+    model: {}
+  };
+}
 
 /**
  * Initialize profiles with default profile if empty
@@ -11,7 +29,22 @@ export const profilesInit = async (): Promise<void> => {
       {
         id: 'default',
         name: 'Default',
-        prompts: DEFAULT_PROMPTS
+        prompts: DEFAULT_PROMPTS,
+        editable: false
+      },
+      {
+        id: 'default2',
+        name: 'Default2',
+        prompts: {
+          version: '1.0.0',
+          generator: 'generator-yeoman',
+          prompts: [
+            getExample(),
+            getExample(),
+            getExample()
+          ]
+        },
+        editable: false
       }
     ];
     await profilesStorageSet(storage);
