880d96e197ad848091f1382d0718244891911c5b
Oliver Grüttner <gruettner@protonmail.com>
2023-04-17 02:00:29 +0200

    feat: add profil logic to the ui

--- src/options/pages/OptionsPagesProfiles.tsx
+++ src/options/pages/OptionsPagesProfiles.tsx
@@ -1,41 +1,102 @@
-import React, { type FC, type ReactNode, useState } from 'react';
+import React, { type FC, type ReactNode, useEffect, useState } from 'react';
 import { Button } from '../../components/Button';
-import { IconLink, IconPackageImport, IconPlus } from '@tabler/icons-react';
+import { IconPackageImport, IconPlus } from '@tabler/icons-react';
 import { ICON_SIZE } from '../../constants';
 import { ProfilesShowPrompts } from './profiles/ProfilesShowPrompts';
-import { ProfilesSourceListsPrompts } from './profiles/ProfilesSourceLists';
 import { ProfilesEdit } from './profiles/ProfilesEdit';
-
-const menuItems: Array<{
-  label: string
-  icon: ReactNode
-  pageContent: ReactNode
-}> = [{
-  label: 'Edit Profile',
-  icon: <IconPackageImport size={ICON_SIZE}/>,
-  pageContent: <ProfilesEdit/>
-}, {
-  label: 'Show Prompts',
-  icon: <IconPackageImport size={ICON_SIZE}/>,
-  pageContent: <ProfilesShowPrompts/>
-}, {
-  label: 'Show Source Lists',
-  icon: <IconLink size={ICON_SIZE}/>,
-  pageContent: <ProfilesSourceListsPrompts/>
-}];
+import { type ProfilesStorage } from '../../types/profilesStorage';
+import { type Prompt } from '../../types/prompt';
+import { profilesStorageGet } from '../../utils/profiles/profilesStorage';
+import { uuid } from '../../utils/uuid';
+import { PromptModal } from '../../components/Promt/PromptModal';
+import { profilesPromptsAdd } from '../../utils/profiles/profilesPrompts';
 
 export interface OptionsPageProfilesProps { }
 
 export const OptionsPagesProfiles: FC<OptionsPageProfilesProps> = () => {
   const [selectedMenuItem, setSelectedMenuItem] = useState(0);
+  const [profiles, setProfiles] = React.useState<ProfilesStorage[]>([]);
+  const [activeProfileId, setActiveProfileId] = React.useState<string | null>(null);
+  const [activePrompts, setActivePrompts] = React.useState<Prompt[]>([]);
 
-  const addPrompt = (): void => {
+  const [activePromptModal, setActivePromptModal] = React.useState<Prompt | null>(null);
+
+  const menuItems: Array<{
+    label: string
+    icon: ReactNode
+    pageContent: ReactNode
+  }> = [{
+    label: 'Show Prompts',
+    icon: <IconPackageImport size={ICON_SIZE}/>,
+    pageContent: <ProfilesShowPrompts prompts={activePrompts} onAddPrompt={() => {
+      setActivePromptModal({
+        id: uuid(),
+        name: 'New Prompt',
+        description: '',
+        metadata: {
+          author: '',
+          creation_date: new Date().toISOString(),
+          source: ''
+        },
+        prompt: '',
+        variables: [],
+        model: '',
+        tags: []
+      });
+    }}/>
+  }, {
+    label: 'Edit Profile',
+    icon: <IconPackageImport size={ICON_SIZE}/>,
+    pageContent: <ProfilesEdit/>
+  }/* {
+    label: 'Show Source Lists',
+    icon: <IconLink size={ICON_SIZE}/>,
+    pageContent: <ProfilesSourceListsPrompts/>
+  } */];
+
+  const openProfile = (id: string): void => {
+    const profile = profiles.find((profile) => profile.id === id);
+    if (profile != null) {
+      setActiveProfileId(profile.id);
+      setActivePrompts(profile.prompts.prompts);
+    }
+  };
+
+  const loadProfiles = (openProfilId?: string): void => {
+    void profilesStorageGet().then((profiles) => {
+      setProfiles(profiles);
+      if (profiles.length === 0) {
+        return;
+      }
+      if (openProfilId === undefined) {
+        setActiveProfileId(profiles[0].id);
+        setActivePrompts(profiles[0].prompts.prompts);
+        return;
+      }
+      const profile = profiles.find((profile) => profile.id === openProfilId);
+      if (profile != null) {
+        setActiveProfileId(profile.id);
+        setActivePrompts(profile.prompts.prompts);
+      }
+    });
+  };
+
+  useEffect(loadProfiles, []);
+
+  const importProfile = (): void => {
     console.log('Import Profile from file'); // todo import profile
   };
 
   return (
+    <>
     <div className={'flex flex-col'}>
-      <div className={'scrollbar flex flex-nowrap overflow-x-auto gap-2 pb-2'}>
+      <div className={'scrollbar flex flex-nowrap overflow-x-auto gap-2 pb-2 text-white'}>
+        <select className={'border border-gray-300 rounded-md bg-blue-500 dark:bg-blue-950 hover:bg-blue-700 pt-1 pl-2 pr-8 h-8'} onChange={(event) => {
+          openProfile(event.target.value);
+        }}>
+          {profiles.map((profile) => (<option className={'select-option active:bg-black'} key={profile.id} value={profile.id}>{profile.name}</option>))}
+        </select>
+
         {menuItems.map((menuItem, index) => (<Button
             key={menuItem.label}
             icon={menuItem.icon}
@@ -46,11 +107,56 @@ export const OptionsPagesProfiles: FC<OptionsPageProfilesProps> = () => {
           >
             {menuItem.label}
           </Button>))}
-        <Button icon={<IconPlus size={ICON_SIZE}/>} onClick={() => { addPrompt(); }}> Import Profile </Button>
+        <Button icon={<IconPlus size={ICON_SIZE}/>} onClick={() => { importProfile(); }}> Import new Profile </Button>
       </div>
       <div className={'flex-grow mt-4'}>
         {menuItems[selectedMenuItem].pageContent}
       </div>
     </div>
+      {activePromptModal !== null && (
+        <PromptModal
+          prompt={activePromptModal}
+          onClose={() => { setActivePromptModal(null); }}
+          onUpdatePrompt={(newPrompt) => {
+            if (activeProfileId === null) {
+              return;
+            }
+            const variables: Array<{
+              name: string
+              type: string
+              description: string
+            }> = [];
+
+            const regex = /{{(.*?)}}/g;
+            let m;
+            while ((m = regex.exec(newPrompt.prompt)) !== null) {
+              if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+              }
+              m.forEach((match, groupIndex) => {
+                if (groupIndex === 1) {
+                  variables.push({
+                    name: match,
+                    type: 'string',
+                    description: ''
+                  });
+                }
+              });
+            }
+
+            if (variables.length > 0) {
+              newPrompt.variables = variables;
+            }
+
+            void profilesPromptsAdd(activeProfileId, {
+              ...newPrompt,
+              variables
+            }).then(() => {
+              loadProfiles(activeProfileId);
+              setActivePromptModal(null);
+            });
+          }} />
+      )}
+      </>
   );
 };
--- src/options/pages/profiles/ProfilesShowPrompts.tsx
+++ src/options/pages/profiles/ProfilesShowPrompts.tsx
@@ -1,46 +1,35 @@
 import React, { type FC } from 'react';
 import { PromptCard, type PromptCardProps } from '../../../components/PromptCard';
+import { type Prompt } from '../../../types/prompt';
+import { Button } from '../../../components/Button';
 
 export interface ProfilesShowPromptsProps {
-
+  prompts: Prompt[]
+  onAddPrompt: () => void
 }
 
-export const ProfilesShowPrompts: FC<ProfilesShowPromptsProps> = () => {
-  const devLoremIpsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
-  const devLoremIpsumArray = devLoremIpsum.split(' ');
-  const prompts: PromptCardProps[] = [
-    {
-      name: 'Test Prompt',
-      description: devLoremIpsum,
-      prompt: devLoremIpsum,
-      variables: devLoremIpsumArray,
-      tags: devLoremIpsumArray,
-      modelName: 'Test Prompt',
-      author: 'Test Prompt',
-      source: 'https://raw.githubusercontent.com/Craifter/oprm/main/examples/examples.oprm'
-    }
-  ];
-
-  for (let i = 0; i < 20; i++) {
-    prompts.push({
-      name: 'Test Prompt',
-      description: devLoremIpsum.slice(0, devLoremIpsum.length - 20 * i),
-      prompt: devLoremIpsum.slice(0, devLoremIpsum.length - 20 * i),
-      variables: devLoremIpsumArray.slice(0, devLoremIpsumArray.length - 10 * i),
-      tags: devLoremIpsumArray.slice(0, devLoremIpsumArray.length - 10 * i),
-      modelName: 'Test Prompt',
-      source: 'https://raw.githubusercontent.com/Craifter/oprm/main/examples/examples.oprm',
-      author: 'Test Prompt'
-    });
-  }
+export const ProfilesShowPrompts: FC<ProfilesShowPromptsProps> = ({
+  prompts,
+  onAddPrompt
+}) => {
+  const cardPrompts: PromptCardProps[] = prompts.map((prompt) => ({
+    name: prompt.name,
+    description: prompt.description,
+    prompt: prompt.prompt,
+    variables: prompt.variables.map((variable) => variable.name),
+    tags: prompt.tags,
+    modelName: prompt.model,
+    author: prompt.metadata.author,
+    source: prompt.metadata.source
+  }));
 
   return (
     <>
-      <div className={'text-xl font-bold dark:text-white mb-4'}>
-        TODO: Search Field/Filters
+      <div className={'text-xl font-bold dark:text-white mb-4 flex'}>
+        <Button onClick={onAddPrompt}>Add Prompt</Button>
       </div>
       <div className={'columns-1 sm:columns-2 lg:columns-3 xl:columns-4 3xl:columns-5 4xl:columns-6 gap-4'}>
-        {prompts.map((prompt, index) => (
+        {cardPrompts.map((prompt, index) => (
           <div key={index} className={'w-full break-inside-avoid-column mb-4'}>
             <PromptCard {...prompt} />
           </div>
